90bfd3da18014482e79b4cfc73a483aa1bddafa0
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 22:41:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
@@ -16,4 +16,27 @@ const App = () => {
 
 
 export default App;
-export {UserContext}
+export {UserContext}*/
+
+import React, { createContext, useState } from 'react';
+import '../styles/App.css';
+import { UserProfile } from './UserProfile';
+
+const UserContext = createContext();
+
+const App = () => {
+	const [user, setUser] = useState({
+		name: 'Newton',
+		age: 3,
+	});
+	return (
+		<UserContext.Provider value={user}>
+			<div id="main">
+				<UserProfile />
+			</div>
+		</UserContext.Provider>
+	);
+};
+
+export default App;
+export { UserContext };
